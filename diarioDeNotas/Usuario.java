ad917cc6c2f2331adf0adec140c699dab68c4eac
BeoliveiraA <163212404+BeoliveiraA@users.noreply.github.com>
2024-05-01 17:23:59 +0000

    Visualizar notas

--- diarioDeNotas/Usuario.java
+++ diarioDeNotas/Usuario.java
@@ -5,20 +5,44 @@ public class Usuario {
     private int idade;
     private String email;
     private String tipoUsuario;
+    private int notaA1 = 70;
+    private int notaA2 = 30;
+    private int notaA3 =40;
 
     Scanner sc = new Scanner(System.in);
+
     public void identificarUsuario() {
-
-    System.out.println("Digite seu nome ");
-    nome = sc.nextLine();
+      System.out.println("Digite seu nome ");
+      nome = sc.nextLine();
     
-    System.out.println("Digite seu email ");
-    email= sc.nextLine();
+      System.out.println("Digite seu email ");
+      email= sc.nextLine();
 
-    System.out.println("Digite sua idade ");
-    idade = sc.nextInt();
+      System.out.println("Digite sua idade ");
+      idade = sc.nextInt();
 
     }
+    public void visualizarNotas(){
+        System.out.println("Escolha a nota que deseja visualizar: ");
+        System.out.println("1. A1");
+        System.out.println("2. A2");
+        System.out.println("3. A3");
+        int escolha = sc.nextInt();
+
+        switch(escolha){
+            case 1:
+                System.out.println("Nota A1: " + notaA1);
+                break;
+            case 2:
+                System.out.println("Nota A2: " + notaA2);
+                break;
+            case 3:
+                System.out.println("Nota A3: " + notaA3);
+                break;
+            default:
+                System.out.println("Opção Inválida " );
+        }
+    } 
 
     String getNome(){
         return nome;
