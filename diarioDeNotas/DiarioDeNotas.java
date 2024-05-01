07c0fbca6435f7bc2a9d9e4655f29922098821d7
BeoliveiraA <163212404+BeoliveiraA@users.noreply.github.com>
2024-05-01 14:56:17 +0000

    correção

--- diarioDeNotas/DiarioDeNotas.java
+++ diarioDeNotas/DiarioDeNotas.java
@@ -2,33 +2,23 @@ import java.util.Scanner;
 
 public class DiarioDeNotas{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Digite 'Aluno' se você for um aluno e 'Professor' se você um professor" );
-        String usuario  = scanner.nextLine();
+        String usuario1  = sc.nextLine();
 
-         if(usuario.equalsIgnoreCase("Professor")){
+         if(usuario1.equalsIgnoreCase("Professor")){
          Professor professor = new Professor();
-         professor.imprimir();
-         professor.identificarProfessor(usuario);
+         professor.identificarUsuario();
 
-         System.out.println("O que tem aqui Get?"  + professor.getNome());
-
-         //professor.somarNotas(nota1, nota2, nota3);
-
-         }else if(usuario.equalsIgnoreCase("Aluno")){
-
-
-
+         }else if(usuario1.equalsIgnoreCase("Aluno")){
             Aluno aluno = new Aluno();
-            String nome = aluno.identificarAluno(usuario);
-
-            System.out.println("Seja bem vindo " + usuario + nome);
+            aluno.identificarUsuario();
 
          }else{
             System.out.println("Opção invalida ");
 
          }
-         scanner.close();
+         sc.close();
     }
   
 
